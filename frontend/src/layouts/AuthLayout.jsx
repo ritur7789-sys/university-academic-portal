d7605807ba2f1a687e8bf537fd77d2c2bccc77e5
Sahil Kumar <sahilkumar@Sahils-MacBook-Air.local>
2026-07-14 15:52:15 +0530

    login register changes

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -1,17 +1,11 @@
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "../styles/auth.css";
 
-function AuthLayout(){
-      
+function AuthLayout() {
+
     return (
         <div className="auth-container">
             <div className="left-panel">
-                <div className="overlay">
-                    <h1>University Academic Portal</h1>
-                    <p>
-                        Empowering Education Through Technology
-                    </p>
-                </div>
             </div>
 
             <div className="right-panel">
